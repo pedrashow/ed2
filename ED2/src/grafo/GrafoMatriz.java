b72a46de7092d72361b3140b4de5fed473232508
roberto oliveira <37985531+pedrashow@users.noreply.github.com>
2019-11-25 17:51:08 -0300

    Update GrafoMatriz.java

--- ED2/src/grafo/GrafoMatriz.java
+++ ED2/src/grafo/GrafoMatriz.java
@@ -27,7 +27,7 @@ public class GrafoMatriz {
 
 	private int mat[][];// pesos do tipo inteiro
 	private int numVertices;
-	private int pos[];// posição atual ao se percorrer os adjs de um vértice v
+	private int pos[];// posiÃ§Ã£o atual ao se percorrer os adjs de um vÃ©rtice v
 	private int numArestas;
 
 	public GrafoMatriz(int numVertices) {
@@ -59,15 +59,15 @@ public class GrafoMatriz {
 	}
 
 	public Aresta primeiroListaAdj(int v) {
-		// Retorna a primeira aresta que o vértice v participa ou
-		// null se a lista de adjacência de v for vazia
+		// Retorna a primeira aresta que o vÃ©rtice v participa ou
+		// null se a lista de adjacÃªncia de v for vazia
 		this.pos[v] = -1;
 		return this.proxAdj(v);
 	}
 
 	public Aresta proxAdj(int v) {
-		// Retorna a próxima aresta que o vértice v participa ou
-		// null se a lista de adjacência de v estiver no fim
+		// Retorna a prÃ³xima aresta que o vÃ©rtice v participa ou
+		// null se a lista de adjacÃªncia de v estiver no fim
 		this.pos[v]++;
 		while ((this.pos[v] < this.numVertices) && (this.mat[v][this.pos[v]] == 0))
 			this.pos[v]++;
@@ -81,7 +81,7 @@ public class GrafoMatriz {
 	public Aresta retiraAresta(int v1, int v2) {
 
 		if (this.mat[v1][v2] == 0)
-			return null; // Aresta não existe
+			return null; // Aresta nÃ£o existe
 		else {
 			Aresta aresta = new Aresta(v1, v2, this.mat[v1][v2]);
 			this.mat[v1][v2] = 0;
@@ -137,7 +137,7 @@ public class GrafoMatriz {
 	public boolean ehCompleto() {
 		for (int i = 0; i < numVertices; i++)
 			for (int j = 0; j < numVertices; j++)
-				if (mat[i][j] == 0)
+				if (mat[i][j] == 0 && i!=j)
 					return false;
 		return true;
 	}
